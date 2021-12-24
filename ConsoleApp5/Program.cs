671f7cbc30be98dc468f80e1621daa227c09cd06
WSR <WSR@DESKTOP-ITM0GG5>
2021-12-24 15:37:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using zadacha_biblioteki;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[4, 8];
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.WriteLine("Введите значение  m[" + i + ", " + j + "]: ");
+                    mas[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            double pow = zadacha_biblioteki.Class1.geo(mas);
+            Console.WriteLine(pow);
+        }
+    }
+}
---
+++ zadacha_biblioteki/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zadacha_biblioteki
+{
+    public class Class1
+    {
+        public static double geo(int[,] mas)
+        {
+            int proiz = 1;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    proiz *= mas[i, j];
+                }
+            }
+            double pow = Math.Pow(proiz, (1.0 / 3.0));
+            return (pow);
+        }
+    }
+}
